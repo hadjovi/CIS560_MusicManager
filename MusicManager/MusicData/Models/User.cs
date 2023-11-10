0cf7b3d88ad74d1ed62106fd7ec0b3833d66fec4
bkeller02 <93346332+bkeller02@users.noreply.github.com>
2023-11-10 17:15:56 -0600

    Created file structure and added model file for User table. Added DataAccess and DataAccessTests projects

---
+++ MusicManager/MusicData/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MusicData.Models
+{
+    public class User
+    {
+        public int UserId { get; }
+        public string Name { get; }
+        public string Email { get; }
+        public string Password { get; }
+
+        public User(int userId, string name, string email, string password)
+        {
+            UserId = userId;
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+    }
+}
